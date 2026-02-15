8adf3fed0392181e5ac529032a2a71b6250f7e8d
skumari8824201-coder <skumari8824201@gmail.com>
2026-02-15 18:20:58 +0530

    authMiddleware.js

---
+++ server/models/server/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ message: "No token" });
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
